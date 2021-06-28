78399364ef604c8947686d54aca8fae14dc289ff
Adham <muzaffarov.adham@gmail.com>
2021-06-28 16:08:16 +0500

    public init

--- src/server.js
+++ src/server.js
@@ -1,4 +1,12 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
 app.listen(8005, () => console.log('server is running on http://localhost:8005'))
