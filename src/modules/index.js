8ab239e645f5cd1dd6b94af97715ab55b75cdc2a
Adham <muzaffarov.adham@gmail.com>
2021-06-28 17:04:08 +0500

    client ended

---
+++ src/modules/index.js
@@ -0,0 +1,6 @@
+const homeRouter = require('./home')
+const loginRouter = require('./login')
+const signupRouter = require('./signup')
+const uploadRouter = require('./upload')
+
+module.exports = [ homeRouter, loginRouter, signupRouter, uploadRouter ] 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
+//express
 const express = require('express')
 const app = express()
 
+//node buildin modules
 const path = require('path')
 
+//middleware
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+//modules
+const modules = require('./modules')
+app.use(modules)
 
 app.listen(8005, () => console.log('server is running on http://localhost:8005'))
